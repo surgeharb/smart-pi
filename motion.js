72b28496bcc602fb2e1c99430439421dcd9f3b8a
surgeharb <me@sergeharb.com>
2017-10-14 20:49:57 +0300

    test

--- motion.js
+++ motion.js
@@ -4,33 +4,41 @@ const board = new five.Board({
   io: new Raspi()
 });
 
+// board.on('ready', () => {
+
+//   // Create a new `motion` hardware instance.
+//   let motion = new five.Motion('P1-7');
+
+//   // 'calibrated' occurs once, at the beginning of a session,
+//   motion.on('calibrated', () => {
+//     console.log('calibrated', Date.now());
+//   });
+
+//   // 'motionstart' events are fired when the 'calibrated'
+//   // proximal area is disrupted, generally by some form of movement
+//   motion.on('motionstart', () => {
+//     console.log('motionstart', Date.now());
+//     const { exec } = require('child_process');
+//     const child = exec('omxplayer sounds/sheyfak.ogg', (error, stdout, stderr) => {
+//       console.log('stdout: ' + stdout);
+//       console.log('stderr: ' + stderr);
+//       if (error !== null) {
+//         console.log('exec error: ' + error);
+//       }
+//     });
+//   });
+
+//   // 'motionend' events are fired following a 'motionstart' event
+//   // when no movement has occurred in X ms
+//   motion.on('motionend', () => {
+//     console.log('motionend', Date.now());
+//   });
+// });
+
 board.on('ready', () => {
 
   // Create a new `motion` hardware instance.
-  let motion = new five.Motion('P1-7');
+  let led = new five.Led('P1-7');
+  led.blink();
 
-  // 'calibrated' occurs once, at the beginning of a session,
-  motion.on('calibrated', () => {
-    console.log('calibrated', Date.now());
-  });
-
-  // 'motionstart' events are fired when the 'calibrated'
-  // proximal area is disrupted, generally by some form of movement
-  motion.on('motionstart', () => {
-    console.log('motionstart', Date.now());
-    const { exec } = require('child_process');
-    const child = exec('omxplayer sounds/sheyfak.ogg', (error, stdout, stderr) => {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-    });
-  });
-
-  // 'motionend' events are fired following a 'motionstart' event
-  // when no movement has occurred in X ms
-  motion.on('motionend', () => {
-    console.log('motionend', Date.now());
-  });
 });
