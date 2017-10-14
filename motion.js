41e5323042ddd6ae4244a8f5cfb0f628d923cc26
surgeharb <me@sergeharb.com>
2017-10-14 20:37:49 +0300

    move pin

--- motion.js
+++ motion.js
@@ -7,7 +7,7 @@ const board = new five.Board({
 board.on('ready', () => {
 
   // Create a new `motion` hardware instance.
-  let motion = new five.Motion('P1-13');
+  let motion = new five.Motion('P1-7');
 
   // 'calibrated' occurs once, at the beginning of a session,
   motion.on('calibrated', () => {
