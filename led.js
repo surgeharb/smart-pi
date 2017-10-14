4a04cfa185dc11687f1c80733d3afee44397fd0c
surgeharb <me@sergeharb.com>
2017-10-14 20:51:49 +0300

    add led

---
+++ led.js
@@ -0,0 +1,13 @@
+const five = require('johnny-five');
+const Raspi = require('raspi-io');
+const board = new five.Board({
+  io: new Raspi()
+});
+
+board.on('ready', () => {
+
+  // Create a new `motion` hardware instance.
+  let led = new five.Led('P1-7');
+  led.blink();
+
+});
