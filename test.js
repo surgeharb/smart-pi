8bdf63636fe0d72ab48d1114a4831b8bd1557870
surgeharb <me@sergeharb.com>
2017-11-12 14:00:34 +0200

    remove logs

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 const { exec } = require('child_process');
 exec('omxplayer sounds/sheyfak.ogg', (error, stdout, stderr) => {
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
   if (error !== null) {
     console.log(`exec error: ${error}`);
   }
