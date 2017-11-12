7d1d2da5e8aff9f5ccf6595121da462af3b84d7d
surgeharb <me@sergeharb.com>
2017-11-12 15:10:28 +0200

    remove test

--- test.js
+++
@@ -1,6 +0,0 @@
-const { exec } = require('child_process');
-exec('omxplayer sounds/sheyfak.ogg', (error, stdout, stderr) => {
-  if (error !== null) {
-    console.log(`exec error: ${error}`);
-  }
-});
