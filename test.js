1a3ecc616d2ce428852d330d8cdcd4cf83729b0f
surgeharb <me@sergeharb.com>
2017-11-12 14:00:05 +0200

    test cild process

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-const portAudio = require('naudiodon');
-
-console.log(portAudio.getDevices());
+const { exec } = require('child_process');
+exec('omxplayer sounds/sheyfak.ogg', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+});
