35207874f058743aaef9276f17fa97ee016ab6e1
surgeharb <me@sergeharb.com>
2017-12-18 23:50:02 +0200

    test

--- libs/sound.js
+++ libs/sound.js
@@ -1,14 +1,26 @@
+const omx = require('omx-layers');
+
+const player = new omx({
+  'disableKeys': true,
+  'audioOutput': 'local',
+  'blackBackground': true,
+  'disableOnScreenDisplay': true
+});
+
+/**
+ * Stop the current song on the device
+ */
+let stop = module.exports.stop = path => {
+  player.stop();
+}
+
 /**
  * Plays the desires song path on the device
  * 
  * @param {String} path Path of the song
  */
 let play = module.exports.play = path => {
-  const { exec } = require('child_process');
-  exec(`omxplayer '${path}'`, (error, stdout, stderr) => {
-    if (error !== null) {
-      console.log(`exec error: ${error}`);
-    }
-    console.log(stdout);
+  stop() && player.open(path, () => {
+    console.log('playback finished!');
   });
 }
