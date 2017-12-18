74f1d2e16770025e66ced2da2c12724ade40e55c
surgeharb <me@sergeharb.com>
2017-12-18 23:25:25 +0200

    fix omxplayer

--- libs/sound.js
+++ libs/sound.js
@@ -5,7 +5,7 @@
  */
 let play = module.exports.play = path => {
   const { exec } = require('child_process');
-  exec(`omxplayer ${path}`, (error, stdout, stderr) => {
+  exec(`omxplayer '${path}'`, (error, stdout, stderr) => {
     if (error !== null) {
       console.log(`exec error: ${error}`);
     }
