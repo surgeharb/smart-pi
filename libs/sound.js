b1cae6534c9254edc7ee20f60760ce76e9a76600
surgeharb <me@sergeharb.com>
2017-12-19 00:35:10 +0200

    add pause

--- libs/sound.js
+++ libs/sound.js
@@ -1,3 +1,12 @@
+let pause = module.exports.pause = () => {
+  const { exec } = require('child_process');
+  exec(`killall omxplayer.bin`, (error, stdout, stderr) => {
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  });
+};
+
 /**
  * Plays the desires song path on the device
  * 
@@ -6,6 +15,7 @@
 let play = module.exports.play = path => {
   const { exec } = require('child_process');
   exec(`omxplayer '${path}'`, (error, stdout, stderr) => {
+    pause();
     if (error !== null) {
       console.log(`exec error: ${error}`);
     }
--- routes/songs.js
+++ routes/songs.js
@@ -12,12 +12,7 @@ const ObjectId = require('mongodb').ObjectID;
 module.exports.routes = (api, database) => {
 
   api.post('/pause', async(request, response, next) => {
-    const { exec } = require('child_process');
-    exec(`killall omxplayer`, (error, stdout, stderr) => {
-      if (error !== null) {
-        console.log(`exec error: ${error}`);
-      }
-    });
+    sound.pause();
     return response.status(200).json({ 'message': 'Success.' });
   });
 
