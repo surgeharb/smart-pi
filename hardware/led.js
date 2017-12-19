43b938e41e090ddae0e789dc9085e1c4e962a62b
surgeharb <me@sergeharb.com>
2017-12-19 12:33:28 +0200

    test

--- hardware/led.js
+++ hardware/led.js
@@ -2,6 +2,7 @@ const CONF = require('../config/config');
 
 module.exports = five => {
   // Create a new `led` hardware instance.
+  console.log(`P1-${CONF.ledsPin}`);
   let led = new five.Led(`P1-${CONF.ledsPin}`);
   CONF.ledController = led;
   console.log('led', led);
